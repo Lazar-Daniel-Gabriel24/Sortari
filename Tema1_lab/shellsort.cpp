ff9dc19e866a1a3e71f25702b33b355152d5e4b2
Lazăr Daniel-Gabriel <116490566+Lazar-Daniel-Gabriel24@users.noreply.github.com>
2023-03-19 16:55:46 +0200

    Update shellsort.cpp

--- Tema1_lab/shellsort.cpp
+++ Tema1_lab/shellsort.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int shellsort(int list[], int n)
+void shellsort(int list[], int n)
 {
     int gap,i,temp,j;
     for(gap = n/2; gap > 0; gap /= 2)
@@ -16,5 +16,5 @@ int shellsort(int list[], int n)
             list[j] = temp;
         }
     }
-    return 0;
+    
 }
